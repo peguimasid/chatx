4e093469635e1c33f441c485167b126a74837535
Guilhermo Masid <guilhermomasid@gmail.com>
2025-05-30 22:19:36 -0300

    chore: back version to fix deployment

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Chatx.MixProject do
     [
       app: :chatx,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
